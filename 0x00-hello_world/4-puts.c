0e052981ae997c8500849bccc1e9ed4377771948
Han-ane <elquorchi.hanane@gmail.com>
2023-06-15 15:57:59 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h> 
 /**
- * main - A program that prints a lin with puts functio 
+ * main - A program that prints a line with puts functio 
 n
  * return: 0 (success)
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
