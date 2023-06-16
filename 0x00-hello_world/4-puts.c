a771b7c36e1a7f5a5b40eed91d890f8408cd5870
Han-ane <elquorchi.hanane@gmail.com>
2023-06-16 12:26:18 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,7 @@ n
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return 0;
 }
