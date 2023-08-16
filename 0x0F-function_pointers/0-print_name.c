2e45bdc95869bc6b62b0503c8c51f5eec421b4e7
Han-ane <elquorchi.hanane@gmail.com>
2023-08-16 15:34:11 -0700

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,15 @@
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - print name using a function pointer
+ * @name: name of the person
+ * @f: function pointer
+ * Return: empty
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
 
-#include <stdlib.h>
-
-int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
+	f(name);
+}
