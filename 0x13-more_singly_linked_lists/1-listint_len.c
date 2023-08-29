4e741980798ad25c510cf0a769ad34069702beb4
Han-ane <elquorchi.hanane@gmail.com>
2023-08-28 18:56:30 -0700

    List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number in a linked lists
+ * @h: pointer the linked list of type listint_t
+ *
+ * Return: 0
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (0);
+}
