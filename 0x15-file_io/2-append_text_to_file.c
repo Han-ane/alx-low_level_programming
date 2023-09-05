e40606e6b369582dd6e96a8b0e6c9ecf4bcf0739
Han-ane <elquorchi.hanane@gmail.com>
2023-09-04 18:33:08 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 			lenght++;
 	}
 	i = open(filename, O_WRONLY | O_APPEND);
-	written = write(o, text_content, lenght);
+	written = write(i, text_content, lenght);
 
 	if (i == -1 || written == -1)
 		return (-1);
